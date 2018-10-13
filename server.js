05355168a1c17d85cce3958052cf95325f815178
Windsvept <junsubjjang@gmail.com>
2018-10-12 18:24:09 -0700

    done

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-require('dotenv').config();
+require('newrelic');
+var compression = require('compression');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port =  3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Proxy server listening on port ${port}...`);
+app.use('/:id',express.static(path.join(__dirname, 'public')));
+
+app.get('/reviews/*', (req, res) => {
+  let id =  req.originalUrl.split('/')[2];
+  axios.get(`http://ec2-18-144-38-130.us-west-1.compute.amazonaws.com:7763/reviews/${id}`)
+  .then(({data})=>{res.send(data)})
+  .catch(({err})=>{res.status(500).send(err)});
 });
+
+app.listen(port);
